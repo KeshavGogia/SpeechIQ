eb1ccc4c2c1a37325e0a14b9093e62fd021483d7
Keshav <keshavgogia57@gmail.com>
2025-05-12 18:53:28 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    allowedDevOrigins: ['http://192.168.1.44'], // Add your IP here
+  },
 }
 
 export default nextConfig
